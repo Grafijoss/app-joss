31a515bd38ebb08efd84077dc5faaa8fa1643e0e
Jose Montenegro <grafijoss@hotmail.com>
2019-02-11 14:21:54 -0500

    hidden buttons

--- src/components/menuicons.js
+++ src/components/menuicons.js
@@ -9,11 +9,23 @@ import { connect } from 'react-redux'
 class Menuicons extends Component {
   /* estado */
   state = {
+		buttons: {
+			bt1: undefined,
+			bt2: undefined,
+			bt3: undefined
+		}
   }
 	/* estado */
 	/* created */
   componentDidMount (prev_props, prev_state) {
 		const _this = this
+		this.setState({
+			buttons: {
+				bt1: document.getElementById('bt1'),
+				bt2: document.getElementById('bt2'),
+				bt3: document.getElementById('bt3')
+			}
+		})
 		setTimeout(() => {
 			_this.fnIni1()
 		}, 1500)
@@ -22,9 +34,9 @@ class Menuicons extends Component {
 	/* methods */
 	/* bt1 */
 	fnIni1 = () => {
-		let bt = document.getElementById('bt1')
-		let cover = bt.getElementsByClassName('cover')
-		let icon = bt.getElementsByTagName('i')
+		// let bt = document.getElementById('bt1')
+		let cover = this.state.buttons.bt1.getElementsByClassName('cover')
+		let icon = this.state.buttons.bt1.getElementsByTagName('i')
 		TweenLite.to(cover, 0.3, { css: {height: '100%', width: '100%', top: 0, left: 0}, ease: Bounce.easeOut, onComplete: () => {
 			this.fnIni2()
 			this.props.dispatch({
@@ -37,9 +49,9 @@ class Menuicons extends Component {
 	/* bt1 */
 	/* bt2 */
 	fnIni2 = () => {
-		let bt = document.getElementById('bt2')
-		let cover = bt.getElementsByClassName('cover')
-		let icon = bt.getElementsByTagName('i')
+		//let bt = document.getElementById('bt2')
+		let cover = this.state.buttons.bt2.getElementsByClassName('cover')
+		let icon = this.state.buttons.bt2.getElementsByTagName('i')
 		// TweenLite.to(cover, 0.6, { ease: Bounce.easeOut, height: '100%', width: '100%', top: 0, left: 0 })
 		TweenLite.to(cover, 0.5, { css: {height: '100%', width: '100%', top: 0, left: 0}, ease: Bounce.easeOut, onComplete: () => {
 			this.fnIni3()
@@ -53,9 +65,9 @@ class Menuicons extends Component {
 	/* bt2 */
 	/* bt3 */
 	fnIni3 = () => {
-		let bt = document.getElementById('bt3')
-		let cover = bt.getElementsByClassName('cover')
-		let icon = bt.getElementsByTagName('i')
+		// let bt = document.getElementById('bt3')
+		let cover = this.state.buttons.bt3.getElementsByClassName('cover')
+		let icon = this.state.buttons.bt3.getElementsByTagName('i')
 		TweenLite.to(cover, 0.5, { css: {height: '100%', width: '100%', top: 0, left: 0}, ease: Bounce.easeOut, onComplete: () => {
 			this.props.dispatch({
 				type: 'ANIMATE_STEP',
@@ -68,7 +80,22 @@ class Menuicons extends Component {
     TweenLite.to(icon, 0.3, { css: {opacity: 1}, delay: 0.4 })
 	}
 	/* bt2 */
+	/* Hidden Buttons*/
+	hiddenButtons = () => {
+		let allButtons = Object.keys(this.state.buttons)
+		allButtons.forEach((key) => {
+			let cover = this.state.buttons[key].getElementsByClassName('cover')
+			let icon = this.state.buttons[key].getElementsByTagName('i')
+			TweenLite.to(cover, 0.5, { css: {height: '0', width: '0', top: '50%', left: '50%'}, ease: Bounce.easeOut, onComplete: () => {
+				this.state.buttons[key].style.display = 'none'
+			}})
+			TweenLite.to(icon, 0.3, { css: {opacity: 0}})
+		})
+	}
+	/* Hidden Buttons*/
+	/* Open Right */
 	openRight = () => {
+		this.hiddenButtons()
 		this.props.dispatch({
 			type: 'OPEN_RIGHT',
 			open: true
@@ -81,15 +108,20 @@ class Menuicons extends Component {
       className="menu-icons">
 				<button 
 				id="bt1"
+				className="main-bt"
 				onClick={this.openRight}>
 					<span className="cover"></span>
 					<i className="fas fa-user-ninja"></i>
 				</button>
-				<button id="bt2">
+				<button 
+				id="bt2"
+				className="main-bt">
 					<span className="cover"></span>
 					<i className="fas fa-file-code"></i>
 				</button>
-				<button id="bt3">
+				<button 
+				id="bt3"
+				className="main-bt">
 					{/* <IconMenu 
 					image={icon3} 
 					nameImage={'bt3'}
